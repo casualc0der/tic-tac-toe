873fe15b2bbfd4e441bb9cc7c2d5cb778cb48fa4
Edd Sansome <edd@murismo.com>
2020-02-23 17:45:28 +0000

    Add click functionailty

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 let gameBoard =(() => {
 
-    let gameboard = [1,2,3,4,5,6,7,8,9]
+    let gameboard = ['','','','','','','','','']
 
     let updatePlayState = (symbol, square) => {
-            gameboard[square] = symbol
+            gameboard[square] = symbol;
     }
-
     let gameBoardDisplay = ()=> gameboard
 
     return { updatePlayState:updatePlayState,
              gameBoardDisplay:gameBoardDisplay }
 })();
 
-//testing that the module is private
-gameBoard.updatePlayState('X', 7);
-console.table(gameBoard.gameBoardDisplay());
-
 let displayController = (() => {
 
     let playArea = document.getElementById('playRender')
     let squares = gameBoard.gameBoardDisplay()
+    let i = 0;
 
     squares.forEach((e) => {
-    let square = document.createElement('div');
-    square.classList= 'playSquares'
-    square.id = `square-${squares.indexOf(e)}`
-    square.innerHTML = e;
-    playArea.appendChild(square)
+        let square = document.createElement('div');
+        square.classList= 'playSquares'
+        square.id = `${i}`
+        square.innerHTML = e;
+        playArea.appendChild(square)
+        i++;
     })
 
+    let reRender = (cell) => {
+        let cellID = document.getElementById(`${cell}`)
+        squares[cell] !== '' ? cellID.innerHTML = squares[cell]: false
+    }
+
+        return {reRender: reRender}
+
 })();
+
+let clicker = (() => {
+
+    let squares = document.querySelectorAll('.playSquares')
+
+    squares.forEach((e) =>  {
+        e.addEventListener('click', ()=> {
+
+            gameBoard.updatePlayState('O',e.id)
+            displayController.reRender(e.id);
+        })
+    })
+})();
+
+
+
+
